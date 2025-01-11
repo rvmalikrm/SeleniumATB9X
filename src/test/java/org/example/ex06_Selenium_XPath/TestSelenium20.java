458547c54d81509454aa9b3153e06bb741f9f1e6
Ravi Malik <rm.rvmalik@gmail.com>
2025-01-11 21:07:20 +0530

    commiting 11  jan 2025 First commit P1

--- src/test/java/org/example/ex06_Selenium_XPath/TestSelenium20.java
+++ src/test/java/org/example/ex06_Selenium_XPath/TestSelenium20.java
@@ -13,40 +13,28 @@ public class TestSelenium20 {
 
     @Description("Verify that with invalid email, pass , error message is showing ")
     @Test
-    public void test_negative_vwo_login() throws Exception,AssertionError {
+    public void test_login_Cura() throws Exception,AssertionError {
 
         EdgeOptions edgeOptions = new EdgeOptions();
-        edgeOptions.addArguments("==stage-maximized==");
+        edgeOptions.addArguments("--stage-maximized");
 
         WebDriver driver =new EdgeDriver(edgeOptions);
         //driver.navigate().to("https://app.vwo.com/#/login");
-        driver.get("https://app.vwo.com/#/login");
+        driver.get("https://katalon-demo-cura.herokuapp.com/");
+        WebElement make = driver.findElement(By.xpath("//a[@id='btn-make-appointment']"));
+        make.click();
+        WebElement username = driver.findElement(By.xpath("//input[@placeholder='Username'][1]"));
+        username.sendKeys("John Dee");
+        WebElement pwd = driver.findElement(By.xpath("//input[@placeholder='Password'][1]"));
+        pwd.sendKeys("ThisisnotPassword");
+
+        WebElement button = driver.findElement(By.xpath("//button[@id='btn-login']"));
+        button.click();
 
 
-        // 1. Find the email input box and enter the email
-        WebElement userId =driver.findElement(By.id("login-username"));
-        userId.sendKeys("admin@admin.com");
-
-        // 2. Find the password inputbox and enter the password
-        WebElement pwd  =driver.findElement(By.id("login-password"));
-        pwd.sendKeys("admin");
-        // 3. find the submit button and click on it
-        WebElement submit = driver.findElement(By.id("js-login-btn"));
-        submit.click();
-        // 4. find the error message and Verify the error massage
-        //<div
-        // class="notification-box-description"
-        // id="js-notification-box-msg"
-        // data-qa="rixawilomi">
-        // Your email, password, IP address or location did not match
-        // </div>
-        Thread.sleep(100);
-        WebElement error_msg=driver.findElement(By.id("js-notification-box-msg"));
-        System.out.println("Actual error message: " +error_msg.getText());
-        Assert.assertEquals(error_msg.getText(),"Your email, password, IP address or location did not match" );
 
 
-    }
+           }
 
 
 }
