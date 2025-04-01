b824781e8d3031b7d274533a5e67bf93f3c0ff52
rvmalikrm <rm.rvmalik@gmail.com>
2025-04-01 14:55:50 +0530

    Commit for Selenium Scroll Down Page

---
+++ src/test/java/Apr25/Lab1Apr25_Automate_Scrolling_on_GUSIndia.java
@@ -0,0 +1,22 @@
+package Apr25;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Lab1Apr25_Automate_Scrolling_on_GUSIndia {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.guseducationindia.com/");
+        driver.manage().window().maximize();
+        WebElement footer=driver.findElement(By.xpath("//a[normalize-space(text())='Terms Conditions']"));
+        JavascriptExecutor js= (JavascriptExecutor) driver;
+        System.out.println(footer);
+        js.executeScript("window.scrollTo(0,document.body.scrollHeight/2)");
+        Thread.sleep(500);
+        js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+//        js.executeScript("argument[0].scrollIntoView(true);",footer);
+}
+    }
