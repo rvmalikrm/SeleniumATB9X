3fa3d610c1aee6680b94d9c82340eceaeb3c1c3c
rvmalikrm <rm.rvmalik@gmail.com>
2025-04-07 19:34:31 +0530

    Commit lat

---
+++ src/test/java/Apr25/Lab7Apr25_DropandDown_Lat.java
@@ -0,0 +1,20 @@
+package Apr25;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Lab7Apr25_DropandDown_Lat {
+    public static void main(String[] args){
+        WebDriver driver =new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.lat.london/");
+        String Tittle=driver.getTitle();
+        System.out.println(Tittle);
+        WebElement ele=driver.findElement(By.cssSelector("form div div button[type='submit']"));
+        JavascriptExecutor js=(JavascriptExecutor)driver;
+        js.executeScript("arguments[0].scrollintoView(true);",ele);
+
+    }
+}
