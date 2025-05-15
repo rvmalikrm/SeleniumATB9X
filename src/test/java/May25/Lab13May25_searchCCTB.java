c1163cc44b96701f7e0d501332852e979e30bc26
rvmalikrm <rm.rvmalik@gmail.com>
2025-05-15 12:09:50 +0530

    Commit forSearchig the text and then scroll it to footer or specific pixel

--- src/test/java/May25/Lab13May25_searchCCTB.java
+++ src/test/java/May25/Lab13May25_searchCCTB.java
@@ -2,16 +2,9 @@ package May25;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
-
 public class Lab13May25_searchCCTB
 {
     WebDriver driver;
@@ -26,27 +19,26 @@ public class Lab13May25_searchCCTB
     public void hoverCCTB() throws InterruptedException {
         WebElement ele1= driver.findElement(By.xpath("//div[@id='searchform']"));
         ele1.click();
-        WebElement ele2 = driver.findElement(By.name("search"));
-        WebDriverWait wait= new WebDriverWait(driver, Duration.ofSeconds(5000));
-        wait.until(ExpectedConditions.elementToBeClickable(By.name("search"))).click();
-        WebDriverWait wait2= new WebDriverWait(driver,Duration.ofSeconds(5000));
-        wait2.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@placeholder='Search...']"))).click();
-        WebDriverWait wait3 =new WebDriverWait(driver,Duration.ofSeconds(2));
-        wait3.until(ExpectedConditions.elementToBeClickable(By.xpath("//form[@class='search-modal-form__wrapper']"))).click();
-        System.out.println("Search field found");
-        WebElement ele4= driver.findElement(By.xpath("//input[@placeholder='Search...']"));
-//        ele4.click();
-        //Actions actions =new Actions(driver);
-        //ele4.sendKeys("Blog");
-        ele4.sendKeys("Blog");
-        ele4.sendKeys(Keys.RETURN);
-        driver.findElement(By.xpath("//button[normalize-space(text())='Seach']")).click();
+        JavascriptExecutor js= (JavascriptExecutor)driver;
+        WebElement searhele= driver.findElement(By.xpath("//form[@class='search-modal-form__wrapper']/div/input"));
+        js.executeScript("arguments[0].click();",searhele);
+        System.out.println("clik success");
+        WebElement searhele2= driver.findElement(By.xpath("//form[@class='search-modal-form__wrapper']/div/input"));
+        JavascriptExecutor js2 =(JavascriptExecutor)driver;
+        js2.executeScript("arguments[0].value='Blog';",searhele2);
+        System.out.println("Locate success");
+        WebElement searcbutton= driver.findElement(By.xpath("//button[normalize-space(text())='Seach']"));
+        JavascriptExecutor js3 =(JavascriptExecutor) driver;
+        js3.executeScript("arguments[0].click();",searcbutton);
+        WebElement footer= driver.findElement(By.xpath("//strong[normalize-space(text())='Others']"));
+        JavascriptExecutor js4=(JavascriptExecutor) driver;
+//        js4.executeScript("arguments[0].scrollIntoView();",footer);
+        js4.executeScript("window.scrollTo(0,1000);");
 
     }
     @AfterMethod
     public void teardown()
     {
-        driver.quit();
-    }
 
+    }
 }
